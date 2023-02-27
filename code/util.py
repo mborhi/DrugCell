de2007de14f34ce0e496ee13bd1953ca8785e266
Marcell Gyorgy Borhi <mborhi@ampere.luddy.indiana.edu>
2023-02-26 22:46:13 -0500

    add reactome classification scripts, utilities

--- code/util.py
+++ code/util.py
@@ -11,8 +11,144 @@ def pearson_corr(x, y):
 
 	return torch.sum(xx*yy) / (torch.norm(xx, 2)*torch.norm(yy,2))
 
-def load_ontology(file_name, gene2id_mapping):
 
+def get_total_genes(term, dG, subsystem_to_genes, total_genes: set):
+    if term in subsystem_to_genes:
+        total_genes = total_genes.union(subsystem_to_genes[term])
+
+    for child in dG.successors(term):
+        get_total_genes(child, dG, subsystem_to_genes, total_genes)
+
+    return total_genes
+
+
+def load_reactome(file_name, gene2id_mapping):
+
+    dG = nx.DiGraph()
+    term_direct_gene_map = {}
+    term_size_map = {}
+
+    file_handle = open(file_name)
+
+    gene_set = set()
+
+    for line in file_handle:
+
+        line = line.rstrip().split()
+        
+        if line[2] == 'default':
+            dG.add_edge(line[0], line[1])
+        else:
+            if line[1] not in gene2id_mapping:
+                continue
+
+            if line[0] not in term_direct_gene_map:
+                term_direct_gene_map[ line[0] ] = set()
+
+            term_direct_gene_map[line[0]].add(gene2id_mapping[line[1]])
+
+            gene_set.add(line[1])
+
+    file_handle.close()
+
+    print('There are', len(gene_set), 'genes')
+
+    min_req_genes = 30
+    min_distinct = 10
+    nodes_to_remove = []
+    for term in dG.nodes():
+        predecessor = [p for p in dG.predecessors(term)]
+        if len(predecessor) == 0:
+            continue
+        predecessor = predecessor[0]
+        deslist = nxadag.descendants(dG, term)
+
+        children_genes = set()
+        child_genes_list = []
+        for child in deslist:
+            child_genes = get_total_genes(
+                child, dG, term_direct_gene_map, set())
+            children_genes = children_genes.union(child_genes)
+            child_genes_list.append(child_genes)
+
+        # term_genes = set()
+        if term in term_direct_gene_map:
+            term_genes = term_direct_gene_map[term]
+            term_genes = term_genes.union(children_genes)
+        else:
+            term_genes = children_genes
+        # has at least min_req_genes in its hierarchy
+        enough_genes = len(term_genes) >= min_req_genes
+
+        # has to have at least min_distinct unique genes in its hierarchy than any child
+        distinct = all(len(term_genes - child_genes) >=
+                        min_distinct for child_genes in child_genes_list)
+
+        term_total_genes = get_total_genes(term, dG, term_direct_gene_map, set())
+        if len(term_total_genes) == 0 or (not (enough_genes and distinct)):
+            nodes_to_remove.append(term)
+            for child in deslist:
+                dG.add_edge(predecessor, child)
+
+    dG.remove_nodes_from(nodes_to_remove)
+
+    leaves = [n for n in dG.nodes if dG.in_degree(n) == 0]
+    #leaves = [n for n,d in dG.in_degree() if d==0]
+
+    uG = dG.to_undirected()
+    connected_subG_list = list(nxacc.connected_components(uG))
+
+    root_node = 'root'
+    edges = [(root_node, n) for n in leaves]
+    dG.add_edges_from(edges)
+
+    print('There are', len(leaves), 'roots:', leaves)
+    print('There are', len(dG.nodes()), 'terms')
+    print('There are', len(connected_subG_list), 'connected componenets')
+
+    leaves = [n for n in dG.nodes if dG.in_degree(n) == 0]
+    uG = dG.to_undirected()
+    connected_subG_list = list(nxacc.connected_components(uG))
+    print("Making adjustments...")
+    print('There are', len(leaves), 'roots:', leaves)
+    print('There are', len(dG.nodes()), 'terms')
+    print('There are', len(connected_subG_list), 'connected componenets')
+
+    for term in dG.nodes():
+        
+        term_gene_set = set()
+
+        if term in term_direct_gene_map:
+            term_gene_set = term_direct_gene_map[term]
+
+        deslist = nxadag.descendants(dG, term)
+
+        for child in deslist:
+            if child in term_direct_gene_map:
+                term_gene_set = term_gene_set | term_direct_gene_map[child] # this is union
+
+        # jisoo
+        if len(term_gene_set) == 0:
+            print("no terms? but there are:", get_total_genes(term, dG, term_direct_gene_map, set()))
+            print('There is empty terms, please delete term:', term)
+            sys.exit(1)
+        else:
+            term_size_map[term] = len(term_gene_set)
+
+    if len(leaves) > 1:
+        print('There are more than 1 root of ontology. Please use only one root.')
+        sys.exit(1)
+    if len(connected_subG_list) > 1:
+        print( 'There are more than connected components. Please connect them.')
+        sys.exit(1)
+
+    return dG, leaves[0], term_size_map, term_direct_gene_map
+
+def load_ontology(file_name, gene2id_mapping):
+	# print("file name to load ont from:", file_name)
+	if "Reactome" in file_name:
+		return load_reactome(file_name, gene2id_mapping)
+	
 	dG = nx.DiGraph()
 	term_direct_gene_map = {}
 	term_size_map = {}
@@ -141,7 +277,7 @@ def load_mapping(mapping_file):
 	file_handle = open(mapping_file)
 
 	for line in file_handle:
-		line = line.rstrip().split()
+		line = line.rstrip().split("\t")# added .split("\t")
 		mapping[line[1]] = int(line[0])
 
 	file_handle.close()
