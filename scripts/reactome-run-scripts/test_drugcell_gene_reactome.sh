bd893bc3ed21579f28e3a05f6696738ef16b3172
Marcell Gyorgy Borhi <mborhi@ampere.luddy.indiana.edu>
2023-02-27 14:49:13 -0500

    add scripts for reactome model with drugcell genes

---
+++ scripts/reactome-run-scripts/test_drugcell_gene_reactome.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+inputdir="../../classification_data/"
+gene2idfile=$inputdir"gene2ind.txt"
+cell2idfile=$inputdir"reactome-sample2ind.txt"
+testdatafile=$inputdir"sample2type_test_ILC_IDC.txt"
+
+mutationfile=$inputdir"sample2binary_mutation.txt"
+
+modelfile="./Reactome_DrugCell_Gene_Model/model_final.pt"
+
+resultdir="Reactome_Classification_Result_sample"
+hiddendir="Reactome_Classification_Hidden_sample"
+
+cudaid=0
+
+mkdir $resultdir
+mkdir $hiddendir
+
+source activate pytorch3drugcell
+
+python -u ../code/predict_drugcell.py -gene2id $gene2idfile -cell2id $cell2idfile -genotype $mutationfile -hidden $hiddendir -result $resultdir -predict $testdatafile -load $modelfile -cuda $cudaid > test_sample.log
---
+++ scripts/reactome-run-scripts/train_drugcell_gene_reactome.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+inputdir="../../classification_data/"
+gene2idfile=$inputdir"gene2ind.txt"
+cell2idfile=$inputdir"reactome-sample2ind.txt" 
+traindatafile=$inputdir"sample2type_test_ILC_IDC.txt"
+valdatafile=$inputdir"sample2type_train_ILC_IDC.txt"
+ontfile=$inputdir"ReactomePathwaysStd.txt"
+
+mutationfile=$inputdir"sample2binary_mutation.txt"
+
+cudaid=0
+
+modeldir=Reactome_DrugCell_Gene_Model
+mkdir $modeldir
+
+source activate pytorch3drugcell
+
+python -u ../code/train_drugcell.py -onto $ontfile -gene2id $gene2idfile -cell2id $cell2idfile -train $traindatafile -test $valdatafile -model $modeldir -cuda $cudaid -genotype $mutationfile -genotype_hiddens 6 -final_hiddens 2 -epoch 100 -batchsize 1000 > train_script.log
