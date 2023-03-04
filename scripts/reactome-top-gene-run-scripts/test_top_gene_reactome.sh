be92f7c0d52f7fb4a28fa83bf4b23cf2f6220c8d
Marcell Gyorgy Borhi <mborhi@ampere.luddy.indiana.edu>
2023-03-04 02:19:11 -0500

    update training, testing scripts

--- scripts/reactome-top-gene-run-scripts/test_top_gene_reactome.sh
+++ scripts/reactome-top-gene-run-scripts/test_top_gene_reactome.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 inputdir="../../classification_data/"
-gene2idfile=$inputdir"gene2ind.txt"
+gene2idfile=$inputdir"top_varied_gene2ind.txt"
 cell2idfile=$inputdir"reactome-sample2ind.txt"
 testdatafile=$inputdir"sample2type_test_ILC_IDC.txt"
 
-mutationfile=$inputdir"sample2binary_mutation.txt"
+mutationfile=$inputdir"top-gene-reactome-sample2binary_mutation.txt"
 
-modelfile="./Reactome_DrugCell_Gene_Model/model_best.pt"
+modelfile="./Reactome_Top_Genes_Model/model_best_corr.pt"
 
-resultdir="Reactome_Classification_Result_sample"
-hiddendir="Reactome_Classification_Hidden_sample"
+resultdir="Reactome_Top_Genes_Classification_Result_sample"
+hiddendir="Reactome_Top_Genes_Classification_Hidden_sample"
 
-cudaid=0
+cudaid=1
 
 mkdir $resultdir
 mkdir $hiddendir
