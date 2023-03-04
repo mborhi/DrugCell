a18d7494e8082015ada0c2c0a74ecc1b4a71eea4
Marcell Gyorgy Borhi <mborhi@ampere.luddy.indiana.edu>
2023-03-04 02:20:56 -0500

    configure misc training, testing scripts

--- scripts/commandline_test_reactome_classification.sh
+++ scripts/commandline_test_reactome_classification.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 inputdir="../classification_data/"
-gene2idfile=$inputdir"reactome-genes2ind.txt" 
+gene2idfile=$inputdir"gene2ind.txt" #"reactome-genes2ind.txt" 
 cell2idfile=$inputdir"reactome-sample2ind.txt"
 testdatafile=$inputdir"sample2type_test_ILC_IDC.txt"
 
-mutationfile=$inputdir"reactome-sample2binary_mutation.txt"
+mutationfile=$inputdir"sample2binary_mutation.txt" # "reactome-sample2binary_mutation.txt"
 
-modelfile="./ClassificationModelTest/model_final.pt"
+modelfile="./ReactomeModelTest/model_final.pt"
 
 resultdir="Reactome_Classification_Result_sample"
 hiddendir="Reactome_Classification_Hidden_sample"
--- scripts/commandline_train_reactome_classification.sh
+++ scripts/commandline_train_reactome_classification.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 inputdir="../classification_data/"
-gene2idfile=$inputdir"reactome-genes2ind.txt" 
+gene2idfile=$inputdir"gene2ind.txt" #"reactome-genes2ind.txt" 
 cell2idfile=$inputdir"reactome-sample2ind.txt" #"sample2ind.txt" #"cell2ind.txt"
 # drug2idfile=$inputdir"drug2ind.txt"
 traindatafile=$inputdir"sample2type_test_ILC_IDC.txt" #"mock_train_data.txt"
 valdatafile=$inputdir"sample2type_train_ILC_IDC.txt" #"mock_test_data.txt"
 ontfile=$inputdir"ReactomePathwaysStd.txt"
 
-mutationfile=$inputdir"reactome-sample2binary_mutation.txt" #"sample2binary_mutation.txt" #"cell2mutation.txt"
+mutationfile=$inputdir"sample2binary_mutation.txt" #"reactome-sample2binary_mutation.txt" #"sample2binary_mutation.txt" #"cell2mutation.txt"
 # drugfile=$inputdir"drug2fingerprint.txt"
 
 cudaid=0
@@ -18,4 +18,4 @@ mkdir $modeldir
 source activate pytorch3drugcell
 
 # python -u ../code/train_drugcell.py -onto $ontfile -gene2id $gene2idfile -drug2id $drug2idfile -cell2id $cell2idfile -train $traindatafile -test $valdatafile -model $modeldir -cuda $cudaid -genotype $mutationfile -fingerprint $drugfile -genotype_hiddens 6 -drug_hiddens '100,50,6' -final_hiddens 6 -epoch 100 -batchsize 5000 > train_script.log
-python -u ../code/train_drugcell.py -onto $ontfile -gene2id $gene2idfile -cell2id $cell2idfile -train $traindatafile -test $valdatafile -model $modeldir -cuda $cudaid -genotype $mutationfile -genotype_hiddens 6 -final_hiddens 2 -epoch 1 -batchsize 10 > train_script.log
+python -u ../code/train_drugcell.py -onto $ontfile -gene2id $gene2idfile -cell2id $cell2idfile -train $traindatafile -test $valdatafile -model $modeldir -cuda $cudaid -genotype $mutationfile -genotype_hiddens 6 -final_hiddens 2 -epoch 50 -batchsize 1000 > train_script.log
